8643c3ed9d95fd0c6f581097ce0667e221c8476e
Thiseas Christos Lamnidis <thisseass@gmail.com>
2021-09-15 15:55:48 +0200

    minor changes

--- build_singularity_with_docker.sh
+++ build_singularity_with_docker.sh
@@ -1,3 +1,2 @@
-rm -f pandora2eager.sif ## Remove built image if existing
 docker pull tclamnidis/singularity-in-docker:3.8.1
-docker run --rm --privileged -v $(pwd):/work tclamnidis/singularity-in-docker:3.8.1 build pandora2eager.sif p2e_singularity.def
+docker run --rm --privileged -v $(pwd):/work tclamnidis/singularity-in-docker:3.8.1 build --force pandora2eager.sif p2e_singularity.def
