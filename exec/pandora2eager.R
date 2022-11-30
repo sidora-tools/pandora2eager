dcf6a45d35b94effc570fa6a4d7f3eb594f4b277
Aida Andrades Valtueña <aida.andrades@gmail.com>
2022-11-30 15:26:23 +0100

    Update exec/pandora2eager.R

--- exec/pandora2eager.R
+++ exec/pandora2eager.R
@@ -84,7 +84,7 @@ collect_and_format_info<- function(query_list_seq, con, file) {
       ) %>%
       mutate(Lane=row_number(), 
       R1=NA, 
-      R2="NA", 
+      R2=NA, 
       analysis.Result_Directory=str_replace(analysis.Result_Directory, "^/projects1", "/mnt/archgen"),
       BAM=paste0(analysis.Result_Directory,sequencing.Full_Sequencing_Id,".bam"), 
       SeqType="SE")
