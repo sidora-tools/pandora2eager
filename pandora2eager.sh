e7ef785daf1a02b1a17ae5bb0caf21f2ae66beca
Thiseas Christos Lamnidis <thisseass@gmail.com>
2022-11-30 15:36:49 +0100

    Add new helptext info

--- pandora2eager.sh
+++ pandora2eager.sh
@@ -10,9 +10,12 @@ function Helptext {
 Usage: pandora2eager.sh [OPTIONS] /path/to/input_seq_IDs_file.txt
 
 Options:
+	-f/--file_type	Specify the file type of the input files. Accepted values are: 'bam', 'fastq_pathogens'.
+			Note: if this flag is not provided, raw fastq will be used to generate the table
 	-r/--rename	Changes all dots (.) in the Library_ID field of the output to underscores (_).
 			Some tools used in nf-core/eager will strip everything after the first dot (.)
 			from the name of the input file, which can cause naming conflicts in rare cases.
+	-d/--debug	Activate debug mode, it produces a file called: 'Debug_table.txt'.
 	-h/--help	Show usage information.
 	-v/--version	Show version information.
 
